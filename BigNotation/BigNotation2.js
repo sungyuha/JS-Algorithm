95e0ee4de538184b0e4aed1c71dc76d45c650885
sungyuha <yuha822@naver.com>
2022-11-23 15:38:29 +0900

    코드 시간 재기

--- BigNotation/BigNotation2.js
+++ BigNotation/BigNotation2.js
@@ -2,5 +2,10 @@ function addUpTo(n) {
     return n * (n + 1)  / 2;
 }
 
-console.log(addUpTo(6))
+//console.log(addUpTo(6))
 // N을 갖고 N+1의 값을 곱하고 2로 나눔
+
+var time1 = performance.now();
+addUpTo(100000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) /1000} seconds.`);
