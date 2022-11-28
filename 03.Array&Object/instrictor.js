8e2483703dc52a60a7b0466acbe9483c4ecdacad
sungyuha <yuha822@naver.com>
2022-11-28 18:45:09 +0900

    객체의 빅오

--- 03.Array&Object/instrictor.js
+++ 03.Array&Object/instrictor.js
@@ -4,3 +4,9 @@ let instructor = {
     favoriteNumbers: [1,2,3,4]
 }
 // 이 객체 리터럴에는 instructor라는 변수에 key value 3개를 저장하고 있음
+// 객체는 정렬되어 있을 필요가 없을 때 잘 작동 됨
+// 빠른 접근, 입력과 제거를 원할 때 좋음
+// 정렬되어 있지 않지만, 다른 것들은 매우 빠름
+/* 객체를 따라오는 메소드 
+    - key, valuse, entries
+*/
